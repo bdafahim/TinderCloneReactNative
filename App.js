36f74f237f19cf825299e6774583be2a993ebc5b
bdafahim <badrudduza20@student.sust.edu>
2020-03-11 16:50:15 +0600

    Card

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Asset, AppLoading } from "expo";
+
+import { Profiles, type Profile } from "./components";
+
+const profiles: Profile[] = [
+    {
+        id: "1",
+        name: "Caroline",
+        age: 24,
+        profile: require("./assets/profiles/1.jpg")
+    },
+    {
+        id: "2",
+        name: "Jack",
+        age: 30,
+        profile: require("./assets/profiles/2.jpg")
+    },
+    {
+        id: "3",
+        name: "Anet",
+        age: 21,
+        profile: require("./assets/profiles/3.jpg")
+    },
+    {
+        id: "4",
+        name: "John",
+        age: 28,
+        profile: require("./assets/profiles/4.jpg")
+    }
+];
 
 type AppState = {
     ready: boolean
---
+++ componenets/index.js
@@ -0,0 +1,2 @@
+export { default as Profiles } from "./Profiles";
+export type { Profile } from "./Profile";
