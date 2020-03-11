5b99112971ac6109fe37ea57d0edb3d2a2d51065
bdafahim <badrudduza20@student.sust.edu>
2020-03-11 15:35:11 +0600

    assets and components added

---
+++ componenets/Profile.js
@@ -0,0 +1,9 @@
+// @flow
+import type { ImageSourcePropType } from "react-native/Libraries/Image/ImageSourcePropType";
+
+export type Profile = {
+    id: String,
+    name: String,
+    age: Number,
+    profile: ImageSourcePropType
+};
